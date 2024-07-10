42956b0ed6898504664271f81dc3f06891810e14
unknown <jusmontenegrope@unal.edu.co>
2024-07-09 22:48:54 -0500

    finished top menu

--- src/Procesador.java
+++ src/Procesador.java
@@ -3,6 +3,7 @@ import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 public class Procesador {
@@ -28,9 +29,33 @@ class LaminaProcesador extends JPanel{
 		setLayout(new BorderLayout());
 		JPanel laminaMenu=new JPanel();
 		JMenuBar miBarra=new JMenuBar();
+		//-------------------------------------
 		JMenu fuente=new JMenu("Fuente");
 		JMenu estilo=new JMenu("Estilo");
 		JMenu tamagno=new JMenu("Tamaño");
+		//-------------------------------------
+		JMenuItem arial=new JMenuItem("Arial");
+		JMenuItem courier=new JMenuItem("Courier");
+		JMenuItem verdana=new JMenuItem("Verdana");
+		fuente.add(arial);
+		fuente.add(courier);
+		fuente.add(verdana);
+		//-------------------------------------
+		JMenuItem negrita=new JMenuItem("Negrita");
+		JMenuItem cursiva=new JMenuItem("Cursica");
+		estilo.add(negrita);
+		estilo.add(cursiva);
+		//-------------------------------------
+		JMenuItem tam12=new JMenuItem("12");
+		JMenuItem tam14=new JMenuItem("14");
+		JMenuItem tam16=new JMenuItem("16");
+		JMenuItem tam18=new JMenuItem("18");
+		tamagno.add(tam12);
+		tamagno.add(tam14);
+		tamagno.add(tam16);
+		tamagno.add(tam18);
+		//-------------------------------------
+
 		miBarra.add(fuente);
 		miBarra.add(estilo);
 		miBarra.add(tamagno);
